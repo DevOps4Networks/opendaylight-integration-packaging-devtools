19aea6d8fa4cc13568bf656120fcce95f3fbe6d9
Nathan Sowatskey <nathan@nathan.to>
2015-11-17 11:53:48 +0100

    Adding build.sh.

---
+++ packer/build.sh
@@ -0,0 +1,1 @@
+rm -rf *.box ~/.vagrant.d/boxes/opendaylight/ .vagrant; packer build -debug -force -var-file=packer_vars.json centos.json;vagrant box add --force --name "opendaylight" opendaylight-3.0.0-centos-1503.box;vagrant destroy --force; vagrant up --provider=virtualbox
